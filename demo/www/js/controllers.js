b7a8d19570c6b9158886a47ee58a54dbcc6318d5
Tim C <timch326@gmail.com>
2015-11-09 11:58:07 -0800

    Created a service that allows the state of the action backdrop to be toggled between hidden and visible states.

--- demo/www/js/controllers.js
+++ demo/www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-    $scope.texts= ['Hello', 'There', 'World', ':)'];
+  .controller('DashCtrl', function ($scope) {
+    $scope.texts = ['Hello', 'There', 'World', ':)'];
 
     $scope.spliceArray = function () {
       $scope.texts.splice(0, 1);
@@ -9,27 +9,33 @@ angular.module('starter.controllers', [])
     }
   })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  .controller('ChatsCtrl', function ($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+      Chats.remove(chat);
+    };
+  })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+  .controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
+  })
+
+  .controller('ActionsCtrl', function ($scope, rgActionTabService) {
+    $scope.trigger = function () {
+      rgActionTabService.toggle();
+    }
+  });
--- js/rgActionBackdropTabItem.js
+++ js/rgActionBackdropTabItem.js
@@ -21,29 +21,44 @@ angular.module('rg.actionTab', [])
         restrict: 'E',
         scope: true,
         require: ['^ionTabs'],
-        link: function (scope, element, attrs, ctrls) {
-          var tabsCtrl = ctrls[0];
-          backgroundEl.children().append(element.children());
-          tabsCtrl.$tabsElement.append($compile(tabNavEl)(scope));
-          tabsCtrl.$tabsElement.append($compile(backgroundEl)(scope));
-          iconEl.addClass(attrs.iconOff);
-          tabNavEl.on('click', toggleBackdrop);
+        compile: function (element) {
 
-          function toggleBackdrop() {
-            if (tabNavEl.hasClass('rg-action-button-active')) {
-              backgroundEl.removeClass('active');
-              backgroundEl.removeClass('visible');
-              tabNavEl.removeClass('rg-action-button-active');
-              iconEl.addClass(attrs.iconOff);
-              iconEl.removeClass(attrs.iconOn);
-            } else {
-              backgroundEl.addClass('active');
-              backgroundEl.addClass('visible');
-              tabNavEl.addClass('rg-action-button-active');
-              iconEl.addClass(attrs.iconOn);
-              iconEl.removeClass(attrs.iconOff);
+          // Pass down action-tab element into the action backdrop without interpolating its
+          // contents until it is appended into a tab element.
+          backgroundEl.children().append(element.children());
+
+          return function link(scope, element, attrs, ctrls) {
+            var tabsCtrl = ctrls[0];
+            tabsCtrl.$tabsElement.append($compile(tabNavEl)(scope));
+            tabsCtrl.$tabsElement.append($compile(backgroundEl)(scope));
+            iconEl.addClass(attrs.iconOff);
+
+            tabNavEl.on('click', toggleBackdrop);
+            scope.$on('rg.toggleBackdrop', toggleBackdrop);
+
+            function toggleBackdrop() {
+              if (tabNavEl.hasClass('rg-action-button-active')) {
+                backgroundEl.removeClass('active');
+                backgroundEl.removeClass('visible');
+                tabNavEl.removeClass('rg-action-button-active');
+                iconEl.addClass(attrs.iconOff);
+                iconEl.removeClass(attrs.iconOn);
+              } else {
+                backgroundEl.addClass('active');
+                backgroundEl.addClass('visible');
+                tabNavEl.addClass('rg-action-button-active');
+                iconEl.addClass(attrs.iconOn);
+                iconEl.removeClass(attrs.iconOff);
+              }
             }
           }
         }
       };
-    }]);
+    }])
+  .service('rgActionTabService', ['$rootScope', function ($rootScope) {
+    return {
+      toggle: function () {
+        $rootScope.$broadcast('rg.toggleBackdrop');
+      }
+    };
+  }]);
